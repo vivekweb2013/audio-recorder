806bf405df7ca2cd7aec98b22fe8e300fa625700
vivekweb2013 <vivekweb2013@gmail.com>
2018-09-01 15:07:56 +0530

    Create base project

---
+++ app/src/main/java/com/wirehall/audiorecorder/MainActivity.java
@@ -0,0 +1,13 @@
+package com.wirehall.audiorecorder;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+}
